b56dfaf905093ee201f10473219038f29611f2d9
calebcheptumo <ckibusia@gmail.com>
2022-05-07 04:22:40 -0400

    import all values

---
+++ app.js
@@ -0,0 +1,4 @@
+const daysEL = document.querySelector('.days')
+const hoursEL = document.querySelector('.hours')
+const minutesEL = document.querySelector('.minutes')
+const secondsEL = document.querySelector('.seconds')
